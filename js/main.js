73afc01d57449980a9afed5627c514c6dd85f1d7
Pinpickle <Pinpickle@abscure.net>
2013-10-24 07:29:45 +1300

    Loads player scores and names into a list

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-var url = "http://95.154.250.152:3000/global-data?token=3b277a3d4852&_=1382502929911", delay = 5000;
+var url = "http://95.154.250.152:3000/global-data?token=93a918c1f3cb", delay = 5000;
 
 function getData() {
     
     $.ajax(url, {
         complete: function() {
-            console.log("Complete");
+            //console.log("Complete");
+            setTimeout(getData, delay);
         },
         success: function(data) {
-            console.log(data);
-            setTimeout(getData, delay);
+            var parsed = JSON.parse(data);
+            var players = parsed.players;
+            //console.log(players);
+            $("#player-scores").html("");
+            for (var i in players) {
+                var player = players[i];
+                $("#player-scores").append("<div>" + player.name + ": " + player.score + "</div>");
+            }
+            
         }
     });
 }

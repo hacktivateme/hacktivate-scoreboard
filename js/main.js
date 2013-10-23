b797aa7253cd2ed43502730ce237a7b789ed4a7d
Pinpickle <Pinpickle@abscure.net>
2013-10-23 17:44:47 +1300

    Getting data from server (broken)
    Due to Access-Control-Allow-Origin not allowing pretty much any domain
    to gather AJAX, this isn't working in browsers that like security
    (pretty much all of them)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var url = "http://95.154.250.152:3000/global-data?token=3b277a3d4852", delay = 5000;
 
+function getData() {
+    
+    $.ajax(url, {
+        complete: function() {
+            alert();
+        },
+        success: function(data) {
+            alert();
+            console.log(data);
+            setTimeout(getData, delay);
+        }
+    });
+}
+
+$(document).ready(function(e) {
+    getData();
+});

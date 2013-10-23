c79f6d21670fee9157809760d8fcafece579bc4f
Pinpickle <Pinpickle@abscure.net>
2013-10-23 18:20:04 +1300

    Getting data from server (broken)
    The server doesn't allow for calls from other domains so this is
    currently running into security issues

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var url = "http://95.154.250.152:3000/global-data?token=3b277a3d4852&_=1382502929911", delay = 5000;
+
+function getData() {
+    
+    $.ajax(url, {
+        complete: function() {
+            console.log("Complete");
+        },
+        success: function(data) {
+            console.log(data);
+            setTimeout(getData, delay);
+        }
+    });
+}
+
+$(document).ready(function(e) {
+    getData();
+});
 
 function getData() {
     

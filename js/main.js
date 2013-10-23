3cc62d9785d02b28ebd8425c66f91d30586ec797
Pinpickle <Pinpickle@abscure.net>
2013-10-23 18:22:26 +1300

    Getting data from server (broken)

--- js/main.js
+++ js/main.js
@@ -17,22 +17,4 @@ $(document).ready(function(e) {
     getData();
 });
 
-<<<<<<< HEAD
-function getData() {
-    
-    $.ajax(url, {
-        complete: function() {
-            console.log("Complete");
-        },
-        success: function(data) {
-            console.log(data);
-            setTimeout(getData, delay);
-        }
-    });
-}
 
-$(document).ready(function(e) {
-    getData();
-});
-=======
->>>>>>> parent of b797aa7... Getting data from server (broken)
